e620adfc375177177bef7c7061666bd41de3c9a9
Philippe Voinov <philippevoinov@gmail.com>
2015-09-14 18:54:02 +0200

    Client registrations over websocket.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 console.log('Started jsCluster coordinator.');
 
@@ -9,4 +10,14 @@ app.get('/', function (req, res) {
 
 http.listen(3210, function () {
   console.log('HTTP up.');
+});
+
+io.on('connection', function (socket) {
+  console.log('Client connected to websocket.');
+  socket.on('disconnect', function () {
+    console.log('Client disconnected from websocket.');
+  });
+  socket.on('registerClient', function (clientInfo) {
+    console.log('Client ' + clientInfo.uuid + ' registered');
+  });
 });
