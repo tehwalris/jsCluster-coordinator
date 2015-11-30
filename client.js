18b5ec1f9128b5d97baa545687c23f0c68eb070b
Philippe Voinov <philippevoinov@gmail.com>
2015-11-30 23:13:01 +0100

    Work unit time logging.

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
+var _ = require('lodash');
 var Q = require('q');
+var TimeLog = require('./timeLog');
 
 class Client {
   constructor (clientInfo, socket) {
@@ -8,9 +10,14 @@ class Client {
 
   run (workUnit) {
     var defered = Q.defer();
+    var timeLog = new TimeLog();
+    timeLog.log('send');
     setTimeout(defered.reject.bind(this, 'Client timed out running work unit.', 'client'), 60000); 
     this.socket.emit('newWorkUnit', workUnit, (response) => {
       if(response.type == 'success') {
+        timeLog.log('receive');
+        workUnit.timeLog = timeLog.exportMillis();
+        _.forEach(response.times, (time, event) => {workUnit.timeLog.push({event: event, time: time});});
         workUnit.result = response.body;
         defered.resolve(response.body);
       } else {
